3ee6bf6bbc0872d5d97d0815e084657c01bad9a6
APCS2nd <APCS2nd@MHS-Dreyer-05>
2018-08-20 09:45:48 -0700

    first project to commit to GitHub fall 2018

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,13 @@
+/**
+ * Print out a message saying hello world.
+ * 
+ * Ben Gu
+ * 8/17/2018
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Ben"); //Display the string
+    }
+}

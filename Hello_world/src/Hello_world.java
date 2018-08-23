a211606fcdc4bbea1bd826561acf10e7d2db1a7f
APCS2nd <APCS2nd@MHS-Dreyer-05>
2018-08-23 09:05:27 -0700

    version 2

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -8,6 +8,6 @@ public class Hello_world
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! From Ben"); //Display the string
+        System.out.println("Hello World! From Ben Gu"); //Display the string
     }
 }

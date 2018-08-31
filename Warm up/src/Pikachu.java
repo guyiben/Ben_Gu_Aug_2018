7c3f967de9503685515bd7e3e993e58c2739ba83
APCS2nd <APCS2nd@MHS-Dreyer-05>
2018-08-31 09:50:02 -0700

    pikachu image

---
+++ Warm up/src/Pikachu.java
@@ -0,0 +1,14 @@
+/**
+ * Print out a Pikachu
+ * 
+ * Ben Gu
+ * 8/31/2018
+ */
+public class Pikachu {
+	public static void main(String[] args) {
+		System.out.println("  (\\__/)");
+		System.out.println("  (o^.^)");
+		System.out.println("z(_(\")(\")");
+	}
+
+}

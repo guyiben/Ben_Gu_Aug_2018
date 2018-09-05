fe62bdb0da1c1c36fad998fb17f27a9661b4f6ff
APCS2nd <APCS2nd@MHS-Dreyer-05>
2018-09-05 09:28:15 -0700

    Here's a Pikachu!

--- Warm up/src/Pikachu.java
+++ Warm up/src/Pikachu.java
@@ -6,8 +6,8 @@
  */
 public class Pikachu {
 	public static void main(String[] args) {
-		System.out.println("  (\\__/)");
-		System.out.println("  (o^.^)");
+		System.out.println("   (\\__/)");
+		System.out.println("   (o^.^)");
 		System.out.println("z(_(\")(\")");
 	}
 
